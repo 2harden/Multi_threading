96f4a85944a067ec3a8a59ae30befd8d51e18575
2harden <zc_shenchn@outlook.com>
2022-11-05 20:26:54 +0800

    update shell script

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #REM Author: 2harden
 #REM Created Time: 2022/11/5
-#REM Release: 1.1
+#REM Release: 1.2
 #REM Description: update commit
+
+#! /usr/bin/bash
 
 echo -n "enter commit msg: "
 read msg
