3ce779715d79620644a12a3f7abcdbb9488ec846
2harden <zc_shenchn@outlook.com>
2022-11-05 08:29:25 +0800

    detach() summery

--- c++/Thread/Thread.cpp
+++ c++/Thread/Thread.cpp
@@ -11,7 +11,15 @@ void MyPrint()
 {
     cout << "thread start..." << endl;
 
-    cout << "thread end..." << endl;
+    cout << "thread end1..." << endl;
+    cout << "thread end2..." << endl;
+    cout << "thread end3..." << endl;
+    cout << "thread end4..." << endl;
+    cout << "thread end5..." << endl;
+    cout << "thread end6..." << endl;
+    cout << "thread end7..." << endl;
+    cout << "thread end8..." << endl;
+    cout << "thread end9..." << endl;
 }
 
 int main()
@@ -38,15 +46,24 @@ int main()
      * detach(): 传统多线程程序主线程要等待子线程执行完毕，然后自己再退出。
      *           detach，分离，也就是主线程不和子线程会合了，你主线程执行你自己的，我子线程执行我的，你主线程也不必等我子线程运行结束，你可以先执行结束，这并不影响我子线程的执行
      *           为什么要引入detach()? 我们创建了很多子线程，让主线逐个等待子线程执行完毕，这种编程方法不太好，所以引入了detech()
+     *           一旦detach()之后，与这个主线程关联的thread对象就会失去与这个主线程的关联，此时这个子线程就会驻留在后台运行（主线程与子线程失去联系）
+     *           这子线程就相当于被c++运行时库接管，当这个子线程执行完毕后，有运行时库负责清理该线程相关的资源（守护线程）
+     *           detach()，会导致MyPrint失去我们自己的控制
      */
 
     // 这里要明确一点，有两个线程在跑，相当于整个程序的执行有两条线在同时走，所以可以干两个事。即使一条线被堵住了，另外一个还是可以通行的，这就是多线程
     // 如果主线程执行完了，但是子线程还没执行完毕，这种程序员是不合格的，写出来的程序也是不稳定的
     // 一个书写良好的程序，应该是主线程等待子线程执行完毕，主线程才能退出
     thread myPrint(MyPrint); // thread：是标准库的一个类，MyPrint：是可调用对象。这一句整体就是创建了线程，线程执行起点（入口）MyPrint(),myPrint线程开始执行
-    myPrint.join(); // 加入线程，说白了就是阻塞，阻塞主线程，让主线程等待子线程执行完毕，然后子线程和主线程会合，一起执行完毕。等待MyPrint()执行完
+    // myPrint.join(); // 加入线程，说白了就是阻塞，阻塞主线程，让主线程等待子线程执行完毕，然后子线程和主线程会合，一起执行完毕。等待MyPrint()执行完
+    myPrint.detach(); // 主/子线程各执行各的，一旦调用了detach()，就再不能用join()，会有异常
 
-    cout << "Hello World!" << endl;
+    cout << "Hello World1!" << endl;
+    cout << "Hello World2!" << endl;
+    cout << "Hello World3!" << endl;
+    cout << "Hello World4!" << endl;
+    cout << "Hello World5!" << endl;
+    cout << "Hello World6!" << endl;
 
     return 0;
 }
---
+++ update.sh
@@ -0,0 +1,9 @@
+@REM Author: 2harden
+@REM Created Time: 2022/11/5
+@REM Release: 1.0
+@REM Description: update commit
+
+
+git add .
+git commit -m "detach() summery"
+git push
