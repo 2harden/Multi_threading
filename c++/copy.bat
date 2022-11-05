e9bf4eb6a1dbd1f2930a8b0d1c3ecdba76f61262
2harden <zc_shenchn@outlook.com>
2022-11-05 10:02:32 +0800

    update description

---
+++ c++/copy.bat
@@ -0,0 +1,12 @@
+@REM Author: 2harden
+@REM Created Time: 2022/11/5
+@REM Release: 1.0
+@REM Description: copy
+
+set srcPath=D:\Development\C++\Thread
+set destPath=D:\GitRepository\Multi_threading\c++\Thread
+
+xcopy /s /e /y %srcPath% %destPath%
+
+pause
+
